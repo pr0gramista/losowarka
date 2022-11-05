5bba97dc4d6ba6e4d639de4490e55968600bd318
Bartosz Wiśniewski <kontakt@pr0gramista.pl>
2022-11-05 15:28:17 +0100

    Add randomizer and a bunch of styles

---
+++ src/randomizer.ts
@@ -0,0 +1,118 @@
+/**
+ * Transformed code to single file.
+ *
+ * Original repository:
+ * https://github.com/durancristhian/meetup-randomizer
+ */
+import * as https from "https";
+import { knuthShuffle } from "knuth-shuffle";
+
+function getEventData(apiUrl, meetupName, eventId) {
+  return new Promise((resolve, reject) => {
+    let body = "";
+    const url = apiUrl
+      .replace("{{meetupName}}", meetupName)
+      .replace("{{eventId}}", eventId);
+
+    https
+      .get(url, (response) => {
+        response.on("data", (chunk) => {
+          body += chunk;
+        });
+
+        response.on("end", () => {
+          const parsedJSON = JSON.parse(body);
+          resolve(parsedJSON);
+        });
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+}
+
+let CUSTOM_API_URL;
+let CUSTOM_PROFILE_URL;
+
+function chooseAWinner(winnersAmount, confirmedMembersList) {
+  return new Promise((resolve, reject) => {
+    if (!confirmedMembersList.length) {
+      return reject(new Error("No qualified attendees available."));
+    }
+
+    if (winnersAmount > confirmedMembersList.length) {
+      return reject(
+        new Error(
+          `The amount of winners (${winnersAmount}) is greater than the amount of confirmed members (${confirmedMembersList.length}).`
+        )
+      );
+    }
+
+    const winners = knuthShuffle(confirmedMembersList.slice(0));
+
+    return resolve(winners.slice(0, winnersAmount));
+  });
+}
+
+function excludeHosts(membersList) {
+  return new Promise((resolve, reject) => {
+    if (Array.isArray(membersList)) {
+      const membersListWithoutHosts = membersList.filter(
+        (m) => !m.member.event_context.host
+      );
+
+      return resolve(membersListWithoutHosts);
+    }
+
+    if (membersList.errors && membersList.errors.length) {
+      const firstError = membersList.errors.shift();
+
+      if (firstError.message) {
+        return reject(new Error(firstError.message));
+      }
+    }
+
+    return reject(new Error("Unknown error encountered with the Meetup API."));
+  });
+}
+
+function excludeNonConfirmedMembers(membersListWithoutHosts) {
+  return new Promise((resolve, reject) => {
+    const confirmedMembersList = membersListWithoutHosts.reduce(
+      (membersList, user) => {
+        if (user.response === "yes") {
+          let photoURL;
+
+          if (user.member.photo) {
+            photoURL =
+              user.member.photo.highres_link || user.member.photo.photo_link;
+          }
+
+          membersList.push({
+            name: user.member.name,
+            photoURL,
+            profileURL:
+              CUSTOM_PROFILE_URL ||
+              `http://www.meetup.com/${user.group.urlname}/members/${user.member.id}`,
+          });
+        }
+
+        return membersList;
+      },
+      []
+    );
+
+    resolve(confirmedMembersList);
+  });
+}
+
+export function run(meetupName, eventId, winnersAmount) {
+  const API_URL =
+    CUSTOM_API_URL ||
+    `https://api.meetup.com/${meetupName}/events/${eventId}/rsvps?only=group.urlname,member,response`;
+
+  return getEventData(API_URL, meetupName, eventId)
+    .then(excludeHosts)
+    .then(excludeNonConfirmedMembers)
+    .then(chooseAWinner.bind(null, winnersAmount));
+}
